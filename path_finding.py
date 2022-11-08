f3468dfd55f241b295d915fdc998895ea2ae74bb
Nafiul Hafiz Abir <30243747+cyraxabir@users.noreply.github.com>
2022-11-08 09:01:54 +0600

    Update path_finding.py

--- path_finding.py
+++ path_finding.py
@@ -1,22 +1,10 @@
-
-try:
-    import pygame
-    import sys
-    import math
-    from tkinter import *
-    from tkinter import ttk
-    from tkinter import messagebox
-    import os
-except:
-    import install_requirements  # install packages
-
-    import pygame
-    import sys
-    import math
-    from tkinter import *
-    from tkinter import ttk
-    from tkinter import messagebox
-    import os
+import pygame
+import sys
+import math
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import os
 
 screen = pygame.display.set_mode((800, 800))
 
